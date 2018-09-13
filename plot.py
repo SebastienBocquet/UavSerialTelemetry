f6309dbf50913b3e9ce095e838872bd70ed00c43
SebastienBocquet <sbocquet@gmail.com>
2018-09-13 23:20:00 +0200

    Refactoring, creation classe Data

---
+++ plot.py
@@ -0,0 +1,149 @@
+import configparser
+import parser_ as pars
+import pyqtgraph as pg
+import sys
+from pyqtgraph.Qt import QtGui, QtCore
+import time
+import numpy as np
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+lineStyleArgs = {}
+lineStyleArgs['solid'] = 'solid'
+lineStyleArgs['dashed'] = QtCore.Qt.DashLine
+lineStyleArgs['dotted'] = QtCore.Qt.DotLine
+
+keys = ['key1', 'key2']  # , 'key3', 'key4']
+legends = ['lgd1', 'lgd2', 'lgd3', 'lgd4']
+
+Config = configparser.ConfigParser()
+Config.read("telemetry.ini")
+serialParams = pars.ConfigSectionMap("Serial", Config)
+displayParams = pars.ConfigSectionMap("Display", Config)
+
+Config.read("default.ini")
+defaultPlotParams = pars.ConfigSectionMap("Plot", Config)
+
+start = time.time()
+
+
+class Data():
+
+    def __init__(self, dummySignal):
+        self.dummySignal = dummySignal
+
+    # Realtime data plot. Each time this function is called, the data display is updated
+    def updateData(self, displayedKey, displayedValue, ptr, ser, line, subLine):
+
+        updated_line = False
+
+        if not self.dummySignal:
+            subLine.acquire(ser.readline())
+            line.concatenate(subLine)
+            if line.endFound:
+                line.convertToDictionary()
+                displayedValue = line.read_value(displayedKey, displayedValue)
+                line.clear()
+                updated_line = True
+            subLine.clear()
+        else:
+            displayedValue = []
+            for i, k in enumerate(displayedKey):
+                displayedValue.append(np.cos(0.05 * ptr) + i)
+
+        logging.debug('\n'+'Displayed values')
+        for key in displayedValue:
+            message = ':'.join([str(key)])
+            logging.debug(message)
+
+        return updated_line, displayedValue
+
+
+class Plot():
+
+    def __init__(self,):
+        self.win = pg.GraphicsWindow(
+            title="Signal from serial port")  # creates a window
+        self.win.resize(1420, 840)
+        self.lineStyle = []
+        self.colors = []
+        self.curves = []
+        self.ptr = 0.
+        self.timeChunkSize = 40
+
+    def setLines(self, defaultPlotParams):
+        assert(defaultPlotParams['maxlineperplot'] <=
+               len(defaultPlotParams['linestyle']))
+        for s in defaultPlotParams['linestyle']:
+            self.lineStyle.append(lineStyleArgs[s])
+
+    def setColors(self, defaultPlotParams):
+        assert(defaultPlotParams['maxlineperplot'] <=
+               len(defaultPlotParams['linecolor']))
+        for c in defaultPlotParams['linecolor']:
+            self.colors.append(c)
+
+    def setCurves(self, displayParams):
+        for i, k in enumerate(keys):
+            plt = self.win.addPlot(title="Realtime plot")
+            plt.addLegend()
+            plt.showGrid(x=True, y=True, alpha=0.5)
+            self.curves.append([])
+            for j, p in enumerate(displayParams[k]):
+                # create an empty "plot" (a curve to plot)
+                self.curves[i].append(
+                    plt.plot(name=displayParams[legends[i]][j], size=18))
+            # CHANGE THE FONT SIZE AND COLOR OF ALL LEGENDS LABEL
+            legendLabelStyle = {'color': '#FFF',
+                                'size': '12pt', 'bold': True, 'italic': False}
+            for item in plt.legend.items:
+                for single_item in item:
+                    if isinstance(single_item, pg.graphicsItems.LabelItem.LabelItem):
+                        single_item.setText(
+                            single_item.text, **legendLabelStyle)
+
+    def checkParams(self, displayParams, defaultPlotParams):
+        assert(len(displayParams['key1']) <=
+               defaultPlotParams['maxlineperplot'])
+
+    def updatePlot(self, Xms, displayedValue):
+
+        # shift data in the temporal mean 1 sample left
+        for xm in Xms:
+            xm[:-1] = xm[1:]
+
+        # vector containing the instantaneous values
+        self.ptr += 1  # update x position for displaying the curve
+        index = 0
+        for p in self.curves:
+            for i, c in enumerate(p):
+                c.setPos(self.ptr, 0)
+                Xms[index][-1] = float(displayedValue[index])
+                if self.lineStyle[i] == 'solid':
+                    c.setData(Xms[index]+1e-16,
+                              pen=pg.mkPen(self.colors[i], width=5))
+                else:
+                    c.setData(Xms[index]+1e-16, pen=pg.mkPen(
+                        self.colors[i], width=5, style=self.lineStyle[i]))
+                # set x position in the graph to 0
+                index += 1
+
+        QtGui.QApplication.processEvents()    # you MUST process the plot now
+
+        return Xms, displayedValue
+
+    def run(self, Xms, displayedKey, displayedValue, data, ser, line, subLine):
+
+        incr = 1
+        while True:
+            Xms, displayedValue = self.updatePlot(Xms, displayedValue)
+            updatedLine, displayedValue = data.updateData(
+                displayedKey, displayedValue, self.ptr, ser, line, subLine)
+            if updatedLine:
+                incr += 1
+                if incr % self.timeChunkSize == 0:
+                    logging.info(
+                        'Reading frequency = %.3f packets per second', incr / (time.time() - start))
+                    incr = 0
+                    start = time.time()
--- real_time_plot.py
+++ real_time_plot.py
@@ -7,139 +7,90 @@ import configparser
 import json
 import parser_ as pars
 import csv_stream_reader as reader
-import time
-import plotFactory as pltF
+import plot as plt
 import logging
 
 logging.basicConfig(level=logging.INFO)
 
 
-Config = configparser.ConfigParser()
-Config.read("telemetry.ini")
-serialParams = pars.ConfigSectionMap("Serial", Config)
-displayParams = pars.ConfigSectionMap("Display", Config)
-
-Config.read("default.ini")
-defaultPlotParams = pars.ConfigSectionMap("Plot", Config)
-
-logging.info('\n'+'Displayed variables')
-for key, value in displayParams.items():
-    message = ':'.join([str(key), str(value)])
-    logging.info(message)
-
-logging.info('\n'+'Parameters used for plotting')
-for key, value in defaultPlotParams.items():
-    message = ':'.join([str(key), str(value)])
-    logging.info(message)
-
-dummySignal = False
-
-# Create object serial port
-try:
-    ser = serial.Serial(serialParams['portname'], serialParams['baudrate'])
-except:
-    logging.info('No data on serial port: generating a dummy signal for testing')
-    dummySignal = True
-
-### START QtApp #####
-# you MUST do this once (initialize things)
-app = QtGui.QApplication([])
-####################
-
-displayedKey = []
-for k in displayParams['key1']:
-    displayedKey.append(k)
-for k in displayParams['key2']:
-    displayedKey.append(k)
-#for k in displayParams['key3']:
-#    displayedKey.append(k)
-#for k in displayParams['key4']:
-#    displayedKey.append(k)
-
-line = reader.Line()
-subLine = reader.SubLine()
-displayedValue = np.empty((len(displayedKey)))
-
-
-# Realtime data plot. Each time this function is called, the data display is updated
-def updateData():
-
-    global displayedValue
-
-    updated_line = False
-
-    if not dummySignal:
-        subLine.acquire(ser.readline())
-        line.concatenate(subLine)
-        if line.endFound:
-            line.convertToDictionary()
-            displayedValue = line.read_value(displayedKey, displayedValue)
-            line.clear()
-            updated_line = True
-        subLine.clear()
-    else:
-        displayedValue = []
-        for i, k in enumerate(displayedKey):
-            displayedValue.append(np.cos(0.05 * ptr) + i)
-
-    logging.debug('\n'+'Displayed values')
-    for key in displayedValue:
-        message = ':'.join([str(key)])
-        logging.debug(message)
-
-    return updated_line
-
-
-def updatePlot():
-
-    global ptr, Xm, displayedValue
-
-    # shift data in the temporal mean 1 sample left
-    for xm in Xms:
-        xm[:-1] = xm[1:]
-
-    # vector containing the instantaneous values
-    ptr += 1 # update x position for displaying the curve
-    index = 0
-    for p in plot.curves:
-        for i, c in enumerate(p):
-            c.setPos(ptr, 0)
-            Xms[index][-1] = float(displayedValue[index])
-            if plot.lineStyle[i] == 'solid':
-                c.setData(Xms[index]+1e-16, pen=pg.mkPen(plot.colors[i], width=5))
-            else:
-                c.setData(Xms[index]+1e-16, pen=pg.mkPen(
-                    plot.colors[i], width=5, style=plot.lineStyle[i]))
-            # set x position in the graph to 0
-            index += 1
-
-    QtGui.QApplication.processEvents()    # you MUST process the plot now
-
-
 if __name__ == '__main__':
 
-    start = time.time()
-    TimeChunkSize = 40
+    Config = configparser.ConfigParser()
+    Config.read("telemetry.ini")
+    serialParams = pars.ConfigSectionMap("Serial", Config)
+    displayParams = pars.ConfigSectionMap("Display", Config)
+
+    Config.read("default.ini")
+    defaultPlotParams = pars.ConfigSectionMap("Plot", Config)
+
+    logging.info('\n'+'Displayed variables')
+    for key, value in displayParams.items():
+        message = ':'.join([str(key), str(value)])
+        logging.info(message)
+
+    logging.info('\n'+'Parameters used for plotting')
+    for key, value in defaultPlotParams.items():
+        message = ':'.join([str(key), str(value)])
+        logging.info(message)
+
+    dummySignal = False
+
+    # Create object serial port
+    try:
+        ser = serial.Serial(serialParams['portname'], serialParams['baudrate'])
+    except:
+        ser = None
+        logging.info('No data on serial port: generating a dummy signal for testing')
+        dummySignal = True
+
+    ### START QtApp #####
+    # you MUST do this once (initialize things)
+    app = QtGui.QApplication([])
+    ####################
+
+    displayedKey = []
+    for k in displayParams['key1']:
+        displayedKey.append(k)
+    for k in displayParams['key2']:
+        displayedKey.append(k)
+    #for k in displayParams['key3']:
+    #    displayedKey.append(k)
+    #for k in displayParams['key4']:
+    #    displayedKey.append(k)
+
+    line = reader.Line()
+    subLine = reader.SubLine()
+
+    data = plt.Data(dummySignal)
+
+    displayedValue = np.empty((len(displayedKey)))
 
     # width of the window displaying the curve
     windowWidth = displayParams['windowwidth']
+
     # create array that will contain the relevant time series
     Xms = []
     for i in range(len(displayedKey)):
         Xms.append(np.linspace(0, 0, windowWidth))
-    ptr = -windowWidth                      # set first x position
 
-    plot = pltF.plot
+    plot = plt.Plot()
+    plot.checkParams(displayParams, defaultPlotParams)
+    plot.setCurves(displayParams)
+    plot.setColors(defaultPlotParams)
+    plot.setLines(defaultPlotParams)
+    windowWidth = displayParams['windowwidth']
+    plot.ptr = -windowWidth
+    plot.run(Xms, displayedKey, displayedValue, data, ser, line, subLine)
 
-    incr = 1
-    while True:
-        updatePlot()
-        updatedLine = updateData()
-        if updatedLine:
-            incr += 1
-            if incr % TimeChunkSize == 0:
-                logging.info('Reading frequency = %.3f packets per second', incr / (time.time() - start))
-                incr = 0
-                start = time.time()
+    # incr = 1
+    # while True:
+    #     displayedValue = plot.updatePlot(Xms, displayedValue)
+    #     updatedLine, displayedValue = updateData(displayedValue, plot.ptr)
+    #     if updatedLine:
+    #         incr += 1
+    #         if incr % TimeChunkSize == 0:
+    #             logging.info('Reading frequency = %.3f packets per second', incr / (time.time() - start))
+    #             incr = 0
+    #             start = time.time()
 
     pg.QtGui.QApplication.exec_()  # you MUST put this at the end
